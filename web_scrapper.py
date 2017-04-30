3c464bc5b447676974b4d26b3669478314e78134
Brian Najera <brian2najera@gmail.com>
2017-04-30 00:02:06 -0500

    search and parse based on search text. for now its barbell

--- web_scrapper.py
+++ web_scrapper.py
@@ -1,11 +1,18 @@
 from selenium import webdriver
 import smtplib
 from email.mime.text import MIMEText
+from struct import *
+
+search = "barbell"
 
 chrome_path = r"D:\Program Files (x86)\PythonStuff\chromedriver.exe"
 driver = webdriver.Chrome(chrome_path)
 driver.get("https://houston.craigslist.org/")
+
 driver.find_element_by_xpath("""//*[@id="sss1"]/li[13]/a""").click()
+searchBox = driver.find_element_by_css_selector("input[placeholder='search sporting goods']")
+searchBox.send_keys(search)
+searchBox.submit()
 
 posts = driver.find_elements_by_css_selector(".result-title.hdrlnk")
 for post in posts:
