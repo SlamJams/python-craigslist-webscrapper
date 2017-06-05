5ef6a58e618813ad1fcf80ce387cf4589387de1a
Brian Najera <brian2najera@gmail.com>
2017-06-04 19:30:08 -0500

    moving from hardcoding the search to having it search the whole site.

--- web_scrapper.py
+++ web_scrapper.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from struct import *
 
 def send_mail(test):
     fromaddr = 'brian2najera@yahoo.com'
@@ -27,27 +26,28 @@ def send_mail(test):
     server.sendmail(login, toaddr, text)
     server.quit()
 
-search = "barbell"
+search = "monitors"
 
 chrome_path = r"D:\Program Files (x86)\PythonStuff\chromedriver.exe"
 driver = webdriver.Chrome(chrome_path)
 driver.get("https://houston.craigslist.org/")
 
-driver.find_element_by_xpath("""//*[@id="sss1"]/li[13]/a""").click()
-searchBox = driver.find_element_by_css_selector("input[placeholder='search sporting goods']")
+driver.find_element_by_xpath("""//*[@id="sss"]/h4/a/span""").click()
+searchBox = driver.find_element_by_css_selector("input[placeholder='search for sale']") # need to change this to not be hard coded to sporting goods.
 searchBox.send_keys(search)
 searchBox.submit()
 
-driver.find_element_by_css_selector("input[name='hasPic']").click() #click to show only posts with images
-driver.find_element_by_css_selector("input[name='postedToday']").click() #click to show only posts submitted today
-driver.find_element_by_css_selector("input[name='bundleDuplicates']").click() #click to bundle duplicates
+driver.find_element_by_css_selector("input[name='hasPic']").click()     #click to show only posts with images
+driver.find_element_by_css_selector("input[name='postedToday']").click()    #click to show only posts submitted today
+driver.find_element_by_css_selector("input[name='bundleDuplicates']").click()    #click to bundle duplicates
+driver.find_element_by_xpath("""//*[@id="searchform"]/div[2]/div/ul/li[2]/a""").click()     #click to show posts from only owners
 
 item = {'title': '', 'price': 0, 'location': '', 'date-posted': '', 'link': '', 'info': ''}
 items = []
 
 results = driver.find_elements_by_css_selector(".result-row")
 
-for result in results:
+for result in results: #parsing info from postings
     item['title'] = result.find_element_by_css_selector('.result-title.hdrlnk').text
     try:
         item['price'] = int(result.find_element_by_css_selector('.result-price').text.strip('$'))
@@ -64,18 +64,17 @@ for result in results:
 for link in items:
     driver.get(link['link'])
     try:
-        driver.find_element_by_xpath('//*[@id="postingbody"]/a').click()
+        driver.find_element_by_xpath('//*[@id="postingbody"]/a').click()   #checks if there is a posting info button to click
     except:
         pass
 
     link['info'] = driver.find_element_by_xpath('//*[@id="postingbody"]').text
 
-print(items)
 
-#posts_price = driver.find_elements_by_css_selector('.result-price')
-#for post in posts_price:
-#    print(post.text)
-
+if not items:
+    print('No new items found.')
+else:
+    print(items)
 
 #send_mail(test)
 driver.close()
