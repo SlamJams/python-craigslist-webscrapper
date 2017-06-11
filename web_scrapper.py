52cab6d1edcd95cdda903b00b09525d488c296c1
Brian Najera <brian2najera@gmail.com>
2017-06-11 01:11:41 -0500

    cleaned up the code a bit.

--- web_scrapper.py
+++ web_scrapper.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 import smtplib
+import re
+import random
+import time
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 def send_mail(items, search):
-    fromaddr = 'brian2najera@yahoo.com'
     toaddr = 'brian2najera@gmail.com'
 
     with open(r'C:\Users\Brian\PycharmProjects\Webscrapper\python-craigslist-webscrapper\info.txt') as f:
@@ -17,9 +19,6 @@ def send_mail(items, search):
     msg['Subject'] = 'Craigslist results'
     msg['From'] = fromaddr
     msg['To'] = toaddr
-    #body = "\n".join(test)
-
-    html_slices = []
     html_slices.append("""
 <!DOCTYPE html>
 <html>
@@ -28,6 +27,14 @@ def send_mail(items, search):
 <link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
 <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
 <script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+<style>
+selector {
+  word-wrap: break-word; /* IE>=5.5 */
+  white-space: pre; /* IE>=6 */
+  white-space: -moz-pre-wrap; /* For Fx<=2 */
+  white-space: pre-wrap; /* Fx>3, Opera>8, Safari>3*/
+}
+</style>
 </head>
 <body>
 
@@ -37,27 +44,26 @@ def send_mail(items, search):
 
     html_slices.append("<h2>{} results for {} </h2>".format(len(items), search))
 
-    for item in items:
+    for item in items: #generating listing html for all the results
         html_slices.append('<div data-role="collapsible">')
         html_slices.append("<h4>{}</h4>".format(item['title']))
         html_slices.append('<ul data-role="listview">')
-        html_slices.append("<li>{}</li>".format(item['info']))
+        html_slices.append("<li>")
+        html_slices.append("{}").format(item['info'])
+        html_slices.append("</li>")
         html_slices.append('</ul>')
         html_slices.append('</div>')
         html_slices.append('\n')
 
     html_slices.append("""
-      </div>
+     </div>
 </div> 
 
 </body>
 </html>
     """)
-
     html = '\n'.join(html_slices)
-
-
-    msg.attach(MIMEText(body, 'plain'))
+    msg.attach(MIMEText(html, 'html'))
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
     server.login(login, password)
@@ -65,14 +71,14 @@ def send_mail(items, search):
     server.sendmail(login, toaddr, text)
     server.quit()
 
-search = "monitors"
+search = "computer monitors gaming"
 
 chrome_path = r"D:\Program Files (x86)\PythonStuff\chromedriver.exe"
 driver = webdriver.Chrome(chrome_path)
 driver.get("https://houston.craigslist.org/")
 
 driver.find_element_by_xpath("""//*[@id="sss"]/h4/a/span""").click()
-searchBox = driver.find_element_by_css_selector("input[placeholder='search for sale']") # need to change this to not be hard coded to sporting goods.
+searchBox = driver.find_element_by_css_selector("input[placeholder='search for sale']")
 searchBox.send_keys(search)
 searchBox.submit()
 
@@ -87,6 +93,7 @@ items = []
 results = driver.find_elements_by_css_selector(".result-row")
 
 for result in results: #parsing info from postings
+    time.sleep(randint(5,10))
     item['title'] = result.find_element_by_css_selector('.result-title.hdrlnk').text
     try:
         item['price'] = int(result.find_element_by_css_selector('.result-price').text.strip('$'))
@@ -102,19 +109,16 @@ for result in results: #parsing info from postings
 
 for link in items:
     driver.get(link['link'])
+    time.sleep(randint(5,15))
     try:
         driver.find_element_by_xpath('//*[@id="postingbody"]/a').click()   #checks if there is a posting info button to click
     except:
         pass
 
     link['info'] = driver.find_element_by_xpath('//*[@id="postingbody"]').text
+    re.sub(r'(\n)', r'\1<br>', link[info])
 
+if items:   #if array has results then send email.
+    send_mail(items, search)
 
-if not items:
-    print('No new items found.')
-else:
-    print(items)
-    print('')
-
-send_mail(items, search)
 driver.close()
