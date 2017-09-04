1180759c31560408541f3b9e76c20e98d486a4f5
Brian Najera <brian2najera@gmail.com>
2017-09-03 21:02:45 -0500

    moved a bunch of html to a file, using an Item class instead of a dictonary to store the craiglist results

--- web_scrapper.py
+++ web_scrapper.py
@@ -1,14 +1,23 @@
-from selenium import webdriver
 import smtplib
-import re
-from random import randint
-import time
-from time import sleep
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-import schedule
 import sqlite3
+import schedule
+import time
 from datetime import datetime, timedelta
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from random import randint
+from time import sleep
+from selenium import webdriver
+
+
+class Item:
+    def __init__(self, title='', location='', date_posted='', link='', info='', price=0):
+        self.title = title
+        self.price = price
+        self.location = location
+        self.date_posted = date_posted
+        self.link = link
+        self.info = info
 
 
 def create_table():
@@ -37,7 +46,7 @@ def check_if_exists(item):
         return False
 
 
-def send_mail(items, search):
+def send_mail(items_list, search):
     toaddr = 'brian2najera@gmail.com'
 
     # opens text file with email username and password in order to login and send mail from it.
@@ -48,104 +57,25 @@ def send_mail(items, search):
         password = ''.join(temp2)
 
     msg = MIMEMultipart()
-    msg['Subject'] = "{} New Results For {}".format(len(items), search)
+    msg['Subject'] = "{} New Results For {}".format(len(items_list), search)
     msg['From'] = login
     msg['To'] = toaddr
     html_slices = []
-    html_slices.append("""
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
-<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-<script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
-<style>
-body{
-  font-family: "Roboto", "Sans Serif";
-  font-size: 18pt;
-  color: #fff;
-  background: #3498db;
-}
 
-.wrapper{
-  margin: 10% auto;
-  width: 400px;
-}
+    f = open(r'D:\Users\Brian\Documents\GitHub\python-craigslist-webscrapper\header.txt')
+    html_slices.append(f.read())
 
-ul{
-  list-style: none;
-  margin: 0;
-  padding: 0;
-}
+    # html_slices.append("<h2>{} results for {} </h2>".format(len(items_list), search))
 
-label{
-  display: block;
-  cursor: pointer;
-  padding: 10px;
-  border: 1px solid #fff;
-  border-bottom: none;
-}
-
-label:hover{
-  background: #26C281;
-}
-
-label.last{
-  border-bottom: 1px solid #fff;
-}
-
-ul ul li{
-  padding: 10px;
-  background: #59ABE3;
-}
-
-
-input[type="checkbox"]{
-  position: absolute;
-  left: -9999px;
-}
-
-input[type="checkbox"] ~ ul{
-  height: 0;
-  transform: scaleY(0);
-}
-
-input[type="checkbox"]:checked ~ ul{
-  height: 100%;
-  transform-origin: top;
-  transition: transform .2s ease-out;
-  transform: scaleY(1); 
-}
-
-input[type="checkbox"]:checked + label{
-  background: #26C281;
-  border-bottom: 1px solid #fff;
-}
-
-selector {
-  word-wrap: break-word; /* IE>=5.5 */
-  white-space: pre; /* IE>=6 */
-  white-space: -moz-pre-wrap; /* For Fx<=2 */
-  white-space: pre-wrap; /* Fx>3, Opera>8, Safari>3*/
-}
-</style>
-</head>
-<body>
-
-<div data-role="page" id="pageone">
-  <div data-role="main" class="ui-content">
-""")
-
-    # html_slices.append("<h2>{} results for {} </h2>".format(len(items), search))
-
-    for item in items:  # generating listing html for all the results
+    for item in items_list:  # generating listing html for all the results
         html_slices.append('<div data-role="collapsible">')
-        html_slices.append("<h4><a href=\"{}\">{}</a></h4>".format(item['link'], item['title']))
-        html_slices.append("<h5><center>Price-{} Location-{} Date Posted-{}</h5>".format(item['price'], item['location'], item['date-posted']))
+        html_slices.append("<h4><a href=\"{}\">{}</a></h4>".format(item.link, item.title))
+        html_slices.append(
+            "<h5><center>Price-{} Location-{} Date Posted-{}</h5>".format(item.price, item.location,
+                                                                          item.date_posted))
         html_slices.append('<ul data-role="listview">')
         html_slices.append("<li>")
-        html_slices.append("{}".format(item['info']))
+        html_slices.append("{}".format(item.info))
         html_slices.append("</li>")
         html_slices.append('</ul>')
         html_slices.append('</div>')
@@ -169,9 +99,7 @@ selector {
 
 
 def main():
-    search = "computer chair"
-    item = {'title': '', 'price': 0, 'location': '', 'date-posted': '', 'link': '', 'info': ''}
-    items = []
+    search = "computer monitors"
 
     chrome_path = r"D:\Program Files (x86)\PythonStuff\chromedriver.exe"
     driver = webdriver.Chrome(chrome_path)
@@ -190,33 +118,33 @@ def main():
     driver.find_element_by_css_selector("input[name='bundleDuplicates']").click()  # click to bundle duplicates
     sleep(randint(1, 3))
     driver.find_element_by_xpath(
-        """//*[@id="searchform"]/div[2]/div/ul/li[2]/a""").click()  # click to show posts from only owners
+        "//*[@id=\"searchform\"]/div[2]/div/ul/li[2]/a").click()  # click to show posts from only owners
 
     results = driver.find_elements_by_css_selector(".result-row")
+    items_list = [Item() for i in range(len(results))]
 
     sleep(randint(5, 10))
-    for result in results:  # parsing info from postings
-        item['title'] = result.find_element_by_css_selector('.result-title.hdrlnk').text
-        if item['title'] != "":  # if title equals an empty string, then its either a duplicate or trash result
+    for i, result in enumerate(results):  # parsing info from postings
+        items_list[i].title = result.find_element_by_css_selector('.result-title.hdrlnk').text
+        if items_list[i].title != "":  # if title equals an empty string, then its either a duplicate or trash result
             try:
-                item['price'] = int(result.find_element_by_css_selector('.result-price').text.strip('$'))
+                items_list[i].price = int(result.find_element_by_css_selector('.result-price').text.strip('$'))
             except:
-                item['price'] = 0
+                items_list[i].price = 0
 
             try:
-                item['location'] = result.find_element_by_css_selector('.result-hood').text
+                items_list[i].location = result.find_element_by_css_selector('.result-hood').text
             except:
-                item['location'] = 'Location not listed.'
+                items_list[i].location = 'Location not listed.'
 
-            item['date-posted'] = result.find_element_by_css_selector('.result-date').text
-            item['link'] = result.find_element_by_css_selector('a').get_attribute('href')
+            items_list[i].date_posted = result.find_element_by_css_selector('.result-date').text
+            items_list[i].link = result.find_element_by_css_selector('a').get_attribute('href')
 
-            if not check_if_exists(item):
-                insert_table(item)
-                items.append(item.copy())
+    # generating a new list without the empty results
+    items_list = [item for item in items_list if item.title != ""]
 
-    for link in items:  # go to each link to grab the description of the item
-        driver.get(link['link'])
+    for item in items_list:  # go to each link to grab the description of the item
+        driver.get(item.link)
         sleep(randint(5, 15))
         try:
             driver.find_element_by_xpath(
@@ -225,10 +153,10 @@ def main():
             pass
 
         str = driver.find_element_by_xpath('//*[@id="postingbody"]').text
-        link['info'] = str.replace("\n", "<br>")
+        item.info = str.replace("\n", "<br>")
 
-    if items:  # if array has results then send email.
-        send_mail(items, search)
+    if items_list:  # if array has results then send email.
+        send_mail(items_list, search)
 
     driver.close()
 
@@ -237,12 +165,13 @@ if __name__ == '__main__':
     try:
         conn = sqlite3.connect('items.db')
         c = conn.cursor()
-    except Error as e:
+    except sqlite3.Error as e:
         print(e)
 
     create_table()
-    schedule.every(5).minutes.do(main)
     main()
-    while 1:
-        schedule.run_pending()
-        time.sleep(1)
+    # schedule.every(1).minutes.do(main)
+    # while 1:
+    #     schedule.run_pending()
+    #     time.sleep(1)
+    #
